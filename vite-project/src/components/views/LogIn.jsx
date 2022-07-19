0c94c9a1251d09c243a7e4d73c2aaaa4e0d03cf4
Andrew <marianoandres191200@gmail.com>
2022-07-18 23:38:53 -0300

    form structure done

--- vite-project/src/components/views/LogIn.jsx
+++ vite-project/src/components/views/LogIn.jsx
@@ -1,10 +1,34 @@
+import { useState } from 'react';
 import '../../styles/login.css';
 
-const Login = () =>{
-    return(
+const Login = () => {
+    const [form, setForm] = useState({})
+
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value 
+        })     
+    }
+
+    const login = (e) =>{
+        e.preventDefault();
+
+        //CALL FIREBASE METHOD
+    }
+
+    return (
         <div className="main-container">
             <div className="login-form-container">
-                <h1>asd</h1>
+                <form className="login-form" onSubmit={(e) => login(e)}>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" name="username" onChange={(e) => handleChange(e)} />
+
+                    <label htmlFor="password">Password</label>
+                    <input type="text" name="password" onChange={(e) => handleChange(e)} />
+
+                    <input type="submit" value="login"/>
+                </form>
             </div>
         </div>
     )
