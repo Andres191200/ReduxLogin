0478062d78f192c2a639d51e3362a3859cb40366
Andrew <marianoandres191200@gmail.com>
2022-07-20 00:14:15 -0300

    fixing transitions when input is filled

--- vite-project/src/components/views/LogIn.jsx
+++ vite-project/src/components/views/LogIn.jsx
@@ -13,26 +13,29 @@ const Login = () => {
 
     const login = (e) => {
         e.preventDefault();
-
+        console.log(form)
         //CALL FIREBASE METHOD
     }
 
     return (
         <div className="main-container">
             <div className="login-form-container">
+                <div>
+                    <h1 className="welcome">¡ Welcome !</h1>
+                </div>
                 <form className="login-form" onSubmit={(e) => login(e)}>
                     <div className="username-password-input-container">
-                        <div className="username-input-container">
-                            <input type="text" name="username" className="username-login" placeholder=" "onChange={(e) => handleChange(e)} />
+                        <div className={`username-input-container ${form.username!=null && form.username.length > 0 && 'filled'}`}>
+                            <input type="text" name="username" className='username-login' placeholder=" " onChange={(e) => handleChange(e)} />
                             <label htmlFor="username">Username</label>
                         </div>
-                        <div className="password-input-container">
-                            <input type="password" name="password" className="password-login" placeholder=" "onChange={(e) => handleChange(e)} />
+                        <div className={`password-input-container ${form.username!=null && form.username.length > 0 && 'filled'}`}>
+                            <input type="password" name="password" className="password-login" placeholder=" " onChange={(e) => handleChange(e)} />
                             <label htmlFor="password">Password</label>
                         </div>
                     </div>
                     <div className="login-button-container">
-                        <input type="submit" value="login" />
+                        <input type="submit" value="LOGIN" />
                     </div>
                 </form>
             </div>
