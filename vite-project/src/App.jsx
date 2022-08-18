9a6549fb17dec1f91b8c6833068ed2fa7c7e2102
Andrew <marianoandres191200@gmail.com>
2022-08-18 20:49:50 -0300

    user dashboard view (/myUser) styled

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,6 +9,7 @@ import MyUser from './components/MyUser';
 function App() {
   const { logged, setLogged } = useIsLogged();
   const navigate = useNavigate();
+  const userInfo = JSON.parse(window.localStorage.getItem('userInfo'));
 
   useEffect(() => {
     logged && navigate('/myUser', { replace: true })
@@ -19,9 +20,9 @@ function App() {
       <div className="App">
 
         <Routes>
-          <Route exact path='/' element={<FormWrapper />} />
-          <Route exact path='/myUser' element={<MyUser />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route exact path='/' element={userInfo ? <MyUser /> : <FormWrapper />} />
+          <Route exact path='/myUser' element={userInfo ? <MyUser /> : <Navigate to="/" replace />} />
+          <Route path="*" element={userInfo ? <Navigate to="/myUser" replace /> : <Navigate to="/" replace />} />
         </Routes>
       </div>
     </Provider>
--- vite-project/src/components/MyUser.jsx
+++ vite-project/src/components/MyUser.jsx
@@ -1,16 +1,21 @@
 import { useContext, useEffect } from "react";
 import UserContext from "../context/userContext";
 import { useNavigate } from "react-router-dom";
+import '../styles/myUser.css';
 
 const MyUser = () => {
     const navigate = useNavigate();
-    const {username} = JSON.parse(window.localStorage.getItem('userInfo'))
+    const { username } = JSON.parse(window.localStorage.getItem('userInfo'))
     useEffect(() => {
         username || navigate('/', { replace: true })
     }, [username])
 
     return (
-        <h1>{username} </h1>  
+        <div className="my-user-main-container">
+            <div className="user-data-container">
+                <h1>Hello {username}</h1>
+            </div>
+        </div>
     )
 }
 
